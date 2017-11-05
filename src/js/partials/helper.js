608783f2ff8e90eaffdf479a2e4f03ea3cd81519
Dmytro Klimchuk <Dmytro.Klimchuk@gmail.com>
2017-11-06 01:26:46 +0200

    finished styling index desktop

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -1,3 +1,26 @@
 window.log = function(param){
     console.log(param);
 };
+
+
+var openFooter = document.querySelector('.keywords__link');
+var keywords = document.querySelector('.keywords');
+
+openFooter.addEventListener('click', function(){
+    if ( keywords.classList.contains ('keywords--opened')) {
+        keywords.classList.remove('keywords--opened');
+    } else {
+        keywords.classList.add('keywords--opened');
+    }
+});
+
+var nav_container = document.querySelector('.nav-container');
+
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > 150) {
+        nav_container.classList.add('f-nav');
+    }else {
+        nav_container.classList.remove('f-nav');
+    }
+}
