996d28aacc5ff8c61adef86f37a83589566b4f9b
Dmytro Klimchuk <Dmytro.Klimchuk@gmail.com>
2017-11-20 01:03:24 +0200

    start price page

--- src/js/partials/helper.js
+++ src/js/partials/helper.js
@@ -58,6 +58,23 @@ $('dl dt').click(function(){
 });
 /**********************end  faq  ****************************/
 
+/**********************  number  ****************************/
+$('.minus').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.plus').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
+/**********************end  number  ****************************/
+
 
 
 
